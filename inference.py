89db18d630100e22eda0f575e988a2e6a4556449
Nexround <527159991@qq.com>
2024-01-28 19:20:18 +0800

    优化推理代码

---
+++ inference.py
@@ -0,0 +1,55 @@
+from ultralytics import YOLO
+from PIL import Image
+from datetime import datetime
+import os
+import argparse
+
+
+def init_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-- origin_dir', type=str)
+    parser.add_argument(
+        '--save_dir', type=str, default='./results/')
+    parser.add_argument(
+        '--model', type=str)
+    args = parser.parse_args()
+    return args
+
+
+def get_formatted_time():
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    return formatted_time
+
+
+def create_folder_if_not_exists(folder_path):
+    # 使用os.path.exists()检测文件夹是否存在
+    if not os.path.exists(folder_path):
+        # 如果不存在，就创建文件夹
+        os.makedirs(folder_path)
+        print(f"文件夹 '{folder_path}' 创建成功.")
+    else:
+        print(f"文件夹 '{folder_path}' 已经存在.")
+
+
+def save_results(results, folder_path):
+
+    for index, item in enumerate(results):
+
+        im_array = item.plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        result_filename = folder_path + '/'+str(index)+'.jpg'
+        print("writing " + result_filename)
+        im.save(result_filename)  # save image
+
+
+if __name__ == '__main__':
+    args = init_parser()
+    folder_path = './results/' + get_formatted_time()
+    create_folder_if_not_exists(folder_path)
+
+    model = YOLO(args.model)
+    results = model(args.origin_dir)
+    save_results(results, folder_path)
+    
